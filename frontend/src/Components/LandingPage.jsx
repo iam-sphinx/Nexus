9899bdfd98864b359a601036d28c8d95ac9680a1
Praval Gautam <pravalgautam@Pravals-MacBook-Air.local>
2024-03-17 18:10:37 +0530

    LandingPage

---
+++ frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className="bg-black h-screen w-screen " >
+        <div className=" h-full flex  justify-center items-center">
+            <img src="/logo.png" className='h-[10rem] w-[10rem]' alt="" />
+    <h1 className='text-[15rem] text-gray-300 p-4 mb-5'>exus</h1>
+    </div>
+        </div>
+  )
+}
+
+export default LandingPage
